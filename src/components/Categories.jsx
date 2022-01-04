e8ad7b86ab7f8c78603a5e7b225adcfb27b009d1
eric bel <ericbelecov@gmail.com>
2022-01-05 01:09:24 +0400

    class component tested

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,6 +1,39 @@
 import React from "react";
 
+// class Categories extends React.Component {
+//   state = {
+//     activeItem: 3,
+//   };
+
+//   onClickItem = (index) => {
+//     this.setState({
+//       activeItem: index,
+//     });
+//   };
+ 
+//   render() {
+//     const { items } = this.props;
+//     return (
+//       <div className="categories">
+//         <ul>
+//           <li>Все</li>
+//           {items.map((name, index) => (
+//             <li
+//               className={this.state.activeItem === index ? "active" : ""}
+//               onClick={() => this.onClickItem(index)}
+//               key={`${name}_${index}`}
+//             >
+//               {name}
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
 const Categories = ({ items, onClick }) => {
+  // const [state, setstate] = useState(initialState)
   return (
     <div className="categories">
       <ul>
