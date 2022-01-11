018e5c1e6fc37d036317c92476a307564bd67351
eric bel <ericbelecov@gmail.com>
2022-01-11 22:12:34 +0400

    after the weekend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import { Header } from "./components";
 import { Home, Cart } from "./components/Pages";
 
 function App() {
+  React.useEffect(() => {}, []);
+
   return (
     <>
       <div className="App">
